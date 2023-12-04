51e97da9f971e62cb513d6fbddce52212a5eeb1d
Ruben <rgarciarios95@gmail.com>
2023-12-04 15:56:07 +0100

    Updated APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,29 +9,31 @@ const App = () => {
         className="object-cover w-full h-full absolute top-0 left-0"
         alt=""
       />
-      <div className="absolute inset-0 flex flex-col items-center justify-center h-full">
-        <div className="text-white flex flex-col items-center justify-center">
-          <img
-            src={image}
-            className="rounded-full h-60 w-60 mb-5 md:mb-0"
-            alt=""
-          />
-          <div className="text-white text-center md:text-left">
+      <div className="absolute inset-0 flex items-center justify-center h-full">
+        <div className="text-white text-center max-w-2xl mx-auto md:flex md:items-center">
+          <div className="md:w-2/3 md:pr-8">
             <h3 className="text-2xl">FullStack Developer</h3>
             <h2 className="text-5xl pt-2 md:pt-5">Rubén García</h2>
-            <p className="pt-2 md:pt-5 text-lg">
-              ¡Hola! 👋 Soy un apasionado desarrollador web con una reciente
-              graduación en Desarrollo de Aplicaciones Web.
+            <p className="pt-2 md:pt-3 text-lg">
+              ¡Hola! 👋 Soy un entusiasta desarrollador, recién graduado en
+              Desarrollo de Aplicaciones Web.
             </p>
-
             <p className="pt-2 md:pt-3 text-lg">
               Me considero un
               <span className="text-amber-400 font-semibold text-xl">
                 {" "}
-                Desarrollador FullStack
+                Desarrollador Junior
               </span>
-              .
+              , explorando el fascinante mundo de la programación y aprendiendo
+              cada día.
             </p>
+          </div>
+          <div className="md:w-1/3">
+            <img
+              src={image}
+              className="rounded-full h-auto w-80 md:ml-10"
+              alt=""
+            />
           </div>
         </div>
       </div>
